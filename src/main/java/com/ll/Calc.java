ae67542440e25ac0c89c342b9dd31ee2898617c8
Sinou3936 <usa3707@naver.com>
2026-04-20 17:28:30 +0900

    TDD ~28 테스트 완료

--- src/main/java/com/ll/Calc.java
+++ src/main/java/com/ll/Calc.java
@@ -2,12 +2,37 @@ package com.ll;
 
 public class Calc {
     public static int run(String expression) {
-        String[] expressionBits = expression.split(" \\+ ");
+        while (expression.contains("(")){
+            int start = expression.lastIndexOf("(");
+            int end = expression.indexOf(")", start);
+            String inner = expression.substring(start + 1, end);
+            int innerResult = run(inner);
 
-        int num1 = Integer.parseInt(expressionBits[0]);
-        int num2 = Integer.parseInt(expressionBits[1]);
+            expression = expression.substring(0, start) + innerResult + expression.substring(end + 1);
 
-        return num1 + num2;
+        }
+
+        String[] arr = expression.split(" ");
+        int result = 0;
+        int lastValue = Integer.parseInt(arr[0]);
+        for (int i = 1; i < arr.length; i+=2) {
+            String operator = arr[i];
+            int operand = Integer.parseInt(arr[i+1]);
+
+            if (operator.equals("+")) {
+                result += lastValue;
+                lastValue = operand;
+            } else if (operator.equals("-")) {
+                result += lastValue;
+                lastValue = -operand;
+            } else if (operator.equals("*")) {
+                lastValue *= operand;
+            } else if (operator.equals("/")) {
+                lastValue /= operand;
+            }
+        }
+        result += lastValue;
+        return result;
     }
 
 }
